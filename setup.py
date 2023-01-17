0f1a331cfda1ece661ac7154d3d1a16e852b32fa
gdefazio <giuseppedefazio90@gmail.com>
2023-01-17 09:51:59 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kMetaShot',
+    version='1.0',
+    packages=['kMetaShot_package'],
+    url='https://github.com/gdefazio/kMetaShot',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Defazio G. et al.',
+    author_email='bruno.fosso@uniba.it',
+    description='kMetaShot is a taxonomy classifier tool relying on an aligmnent free and k-mer/minimizer based approach.'
+)
